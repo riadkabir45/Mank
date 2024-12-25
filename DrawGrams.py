f67fe0c1dcc409b0d8f8b60236bc19332d760bed
riadkabir45 <riadkabir45@gmail.com>
2024-12-25 11:43:53 +0600

    feat: basic maze

---
+++ DrawGrams.py
@@ -0,0 +1,143 @@
+from enum import Enum
+Zone = Enum("Zone",[ (f"z{zone}",zone) for zone in range(9) ])
+
+def draw_mpc(cx,cy,r):
+    nPoint = []
+    d = 1 - r
+    x = r
+    y = 0
+    nPoint.extend(drawAllZone(x,y,cx,cy))
+    while x >= y:
+        if d > 0:
+            d += 2*y - 2*x + 5
+            x -= 1
+            y += 1
+        else:
+            d += 2*y + 1
+            y += 1
+        nPoint.extend(drawAllZone(x,y,cx,cy))
+    
+    return nPoint
+
+
+def drawAllZone(x,y,cx,cy):
+    nPoint = []
+    for rev in [True,False]:
+        for my in [-1,1]:
+            for mx in [-1,1]:
+                point = list(coeffer(x,y,(mx,my,rev)))
+                point[0] += cx
+                point[1] += cy
+                nPoint.append(point)
+    return nPoint
+
+def draw_mpl(x1,y1,x2,y2):
+    nPoints = []
+    
+    size = 1
+    zone = find_zone(x1,y1,x2,y2)
+    if zone == Zone.z0:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(1,1,False))
+    elif zone == Zone.z1:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(1,1,True))
+    elif zone == Zone.z2:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(1,-1,True))
+    elif zone == Zone.z3:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(-1,1,False))
+    elif zone == Zone.z4:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(-1,-1,False))
+    elif zone == Zone.z5:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(-1,-1,True))
+    elif zone == Zone.z6:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(-1,1,True))
+    else:
+        x1,y1,x2,y2 = coeffer2d(x1,y1,x2,y2,(1,-1,True))
+
+    points = draw_mpl_z0(x1,y1,x2,y2)
+
+    for point in points:
+        if zone == Zone.z0:
+            point = coeffer(point[0],point[1],(1,1,False))
+            nPoints.append((point[0],point[1],size))
+        elif zone == Zone.z1:
+            point = coeffer(point[0],point[1],(1,1,True))
+            nPoints.append((point[0],point[1],size))
+        elif zone == Zone.z2:
+            point = coeffer(point[0],point[1],(-1,1,True))
+            nPoints.append((point[0],point[1],size))
+        elif zone == Zone.z3:
+            point = coeffer(point[0],point[1],(-1,1,False))
+            nPoints.append((point[0],point[1],size))
+        elif zone == Zone.z4:
+            point = coeffer(point[0],point[1],(-1,-1,False))
+            nPoints.append((point[0],point[1],size))
+        elif zone == Zone.z5:
+            point = coeffer(point[0],point[1],(-1,-1,True))
+            nPoints.append((point[0],point[1],size))
+        elif zone == Zone.z6:
+            point = coeffer(point[0],point[1],(1,-1,True))
+            nPoints.append((point[0],point[1],size))
+        else:
+            point = coeffer(point[0],point[1],(1,-1,False))
+            nPoints.append((point[0],point[1],size))
+
+    return nPoints
+
+def coeffer2d(x1,y1,x2,y2,multiplier):
+    x1, y1 = coeffer(x1,y1,multiplier)
+    x2, y2 = coeffer(x2,y2,multiplier)
+    return (x1,y1,x2,y2)
+
+def coeffer(x1,y1,multiplier):
+    mx, my, rev = multiplier
+    if rev:
+        x1,y1 = y1,x1
+    return (mx*x1,my*y1)
+
+def draw_mpl_z0(x1,y1,x2,y2):
+    points = []
+
+    dx = x2 - x1
+    dy = y2 - y1
+    d = 2*dy - dx
+    dE = 2*dy
+    dNE = 2*(dy - dx)
+    x = x1
+    y = y1
+
+    points.append((x,y))
+    while(x <= x2):
+        if d <= 0:
+            x += 1
+            d += dE
+        else:
+            x += 1
+            y += 1
+            d += dNE
+        points.append((x,y))
+    return points
+
+
+
+def find_zone(x1,y1,x2,y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    adx = abs(dx)
+    ady = abs(dy)
+    if adx > ady:
+        if dx < 0:
+            if dy < 0:
+                return Zone.z4
+            return Zone.z3
+        if dy < 0:
+            return Zone.z7
+        return Zone.z0
+    else:
+        if dx < 0:
+            if dy < 0:
+                return Zone.z5
+            return Zone.z2
+        if dy < 0:
+            return Zone.z6
+        return Zone.z1
+
---
+++ main.py
@@ -0,0 +1,175 @@
+from OpenGL.GL import *  
+from OpenGL.GLUT import *  
+from OpenGL.GLU import *
+from random import randint
+
+from DrawGrams import draw_mpc, draw_mpl
+  
+SCREEN_W, SCREEN_H = 500, 500
+cells = 10
+
+BLOCK_SIZE_W = int(SCREEN_W/cells)
+BLOCK_SIZE_H = int(SCREEN_H/cells)
+
+map_state = []
+node_paths = []
+
+def draw_array(aPoint):
+    glPointSize(1)
+    glBegin(GL_POINTS)
+    for point in aPoint:
+        glVertex2f(point[0],point[1])
+    glEnd()
+
+def rgb(r,g,b):
+    glColor3f(r/255, g/256, b/256)
+
+def mage_cell(x,y):
+    x *= BLOCK_SIZE_W
+    y *= BLOCK_SIZE_H
+    return draw_rectangle(x,y,x+BLOCK_SIZE_W,y+BLOCK_SIZE_H)
+
+def create_path(x,y,dx,dy):
+    nPoints = []
+    if dx > 0:
+        x *= BLOCK_SIZE_W
+        y *= BLOCK_SIZE_H
+        nPoints.extend(fill_rectangle(x+BLOCK_SIZE_W-1,y+1,x+BLOCK_SIZE_W+2,y+BLOCK_SIZE_H-1))
+    elif dx < 0:
+        x -= 1
+        x *= BLOCK_SIZE_W
+        y *= BLOCK_SIZE_H
+        nPoints.extend(fill_rectangle(x+BLOCK_SIZE_W-1,y+1,x+BLOCK_SIZE_W+2,y+BLOCK_SIZE_H-1))
+    elif dy > 0:
+        x *= BLOCK_SIZE_W
+        y *= BLOCK_SIZE_H
+        nPoints.extend(fill_rectangle(x+1,y+BLOCK_SIZE_H-1,x+BLOCK_SIZE_W-2,y+BLOCK_SIZE_H+1))
+    elif dy < 0:
+        y -= 1
+        x *= BLOCK_SIZE_W
+        y *= BLOCK_SIZE_H
+        nPoints.extend(fill_rectangle(x+1,y+BLOCK_SIZE_H-1,x+BLOCK_SIZE_W-2,y+BLOCK_SIZE_H+1))
+    return nPoints
+
+    return nPoints
+
+def fill_rectangle(x1,y1,x2,y2):
+    nPoints = []
+    if y2 < y1:
+        y1, y2 = y2, y1
+    for y in range(y1,y2+1):
+        nPoints.extend(draw_mpl(x1,y,x2,y))
+    return nPoints
+
+def initMap():
+    for y in range(cells):
+        for x in range(cells):
+                map_state.extend(mage_cell(x,y))
+    
+    cMap = map_state[:]
+    map_state.clear()
+    map_state.extend(sorted(list(set(cMap))))
+    
+    paths = generateMage()
+    for path in paths:
+        mx, my = path[0]
+        dx, dy = path[1][0] - mx, path[1][1] - my
+        tp = create_path(mx,my,dx,dy)
+        for point in tp:
+            sPoint = (point[0],point[1],1)
+            if sPoint in map_state:
+                res = map_state.remove(sPoint)
+
+def ranDir():
+    path = [-1,0,1]
+    npath = []
+    while len(npath) < 3:
+        rg = len(path) - 1
+        if rg >= 1:
+            pos = randint(0,rg)
+        else:
+            pos = 0
+        val = path.pop(pos)
+        npath.append(val)
+    return npath
+
+def unexplored_cell(map,x,y):
+    for dy in ranDir():
+        for dx in ranDir():
+            if dx == dy == 0 or abs(dx) == abs(dy):
+                continue
+            px, py = x+dx, y+dy
+            if py >= len(map) or px >= len(map[py]):
+                continue
+            if py < 0 or px < 0:
+                continue
+            if map[px][py] == 0:
+                return (x+dx,y+dy)
+            
+
+def generateMage():
+    map = [[0] * cells for _ in range(cells)]
+    np = (0,0)
+    paths = []
+    stack = []
+    points = []
+    while True:
+        map[np[0]][np[1]] = 1
+        cp = np[:]
+        points.append(cp)
+        np = unexplored_cell(map,np[0],np[1])
+        if np is not None:
+            stack.append((cp,np))
+            paths.append((cp,np))
+        while np is None and len(stack) > 0:
+            np = stack.pop()[0]
+            continue
+        if np is None:
+            break
+    node_paths.clear()
+    node_paths.extend(paths)
+    return paths
+
+def draw_rectangle(x1,y1,x2,y2):
+    nPoints = []
+    nPoints.extend(draw_mpl(x1,y1,x1,y2))
+    nPoints.extend(draw_mpl(x1,y1,x2,y1))
+    nPoints.extend(draw_mpl(x2,y2,x2,y1))
+    nPoints.extend(draw_mpl(x2,y2,x1,y2))
+    return nPoints
+  
+def iterate():  
+    glViewport(0, 0, SCREEN_W, SCREEN_H)  
+    glMatrixMode(GL_PROJECTION)  
+    glLoadIdentity()  
+    glOrtho(0.0, SCREEN_W+cells/10, 0.0, SCREEN_H+cells/10, 0.0, 1.0)  
+    glMatrixMode (GL_MODELVIEW)  
+    glLoadIdentity()  
+  
+def showScreen():  
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  
+    glLoadIdentity()  
+    iterate() 
+
+    rgb(255,255,255)
+    draw_array(map_state)
+    
+
+    glutSwapBuffers()  
+    glutPostRedisplay()
+
+def animate():
+    pass
+
+initMap()
+
+glutInit()  
+glutInitDisplayMode(GLUT_RGBA)  
+glutInitWindowSize(SCREEN_W, SCREEN_H)  
+glutInitWindowPosition(0, 0)  
+wind = glutCreateWindow("Mank")  
+glutDisplayFunc(showScreen)  
+glutIdleFunc(animate)  
+glutMainLoop()  
+#generateMage()
+#print(ranDir())
